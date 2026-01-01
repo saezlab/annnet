0a1e09bd811041584e782569529648615509901e
l1boll <ynaciri808@gmail.com>
2026-01-01 20:00:58 +0100

    renamed io/utils to io/_utils + added helper functions for .annnet file

---
+++ annnet/io/_utils.py
@@ -0,0 +1,15 @@
+# Placeholder for utils.py
+
+def _write_archive(src_dir: Path, outfile: Path):
+    with tarfile.open(outfile, "w:gz") as tar:
+        tar.add(src_dir, arcname=src_dir.name)
+
+
+def _read_archive(infile: Path, tmpdir: Path) -> Path:
+    with tarfile.open(infile, "r:gz") as tar:
+        tar.extractall(tmpdir)
+
+    roots = list(tmpdir.iterdir())
+    if len(roots) != 1:
+        raise ValueError("Invalid annnet archive: expected single root directory")
+    return roots[0]
