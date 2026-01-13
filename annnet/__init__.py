7e833a4096f081eb9177e95658a016ec49075e36
l1boll <ynaciri808@gmail.com>
2026-01-13 18:35:29 +0100

    fix exposed API in __init__

--- annnet/__init__.py
+++ annnet/__init__.py
@@ -17,18 +17,20 @@ _lazy_submodules = {
     "algorithms": "annnet.algorithms",
     "utils": "annnet.utils",
     # adapter modules (direct convenience)
-    "graphml": "annnet.adapters.GraphML_adapter",
-    "sif": "annnet.adapters.SIF_adapter",
-    "sbml": "annnet.adapters.sbml_adapter",
-    "parquet": "annnet.adapters.GraphDir_Parquet_adapter",
     "networkx": "annnet.adapters.networkx_adapter",
     "igraph": "annnet.adapters.igraph_adapter",
-    "jsonio": "annnet.adapters.json_adapter",
-    "dataframe": "annnet.adapters.dataframe_adapter",
+    "graphtool": "annnet.adapters.graphtool_adapter",
+    "PyGeometric": "annnet.adapters.pyg_adapter",
     # io modules
     "csvio": "annnet.io.csv",
-    "excelio": "annnet.io.excel",
+    "excel": "annnet.io.excel",
     "annnet": "annnet.io.io_annnet",
+    "json": "annnet.io.json_io",
+    "dataframe": "annnet.io.dataframe_io",
+    "graphml": "annnet.io.GraphML_io",
+    "sif": "annnet.io.SIF_io",
+    "sbml": "annnet.io.sbml_io",
+    "parquet": "annnet.io.GraphDir_Parquet_io",
 }
 
 # Curated top-level symbols (lazy). name -> (module, attribute)
@@ -38,28 +40,42 @@ _lazy_symbols: dict[str, tuple[str, str]] = {
     # Stdlib JSON I/O
     "to_json": ("annnet.adapters.json_adapter", "to_json"),
     "from_json": ("annnet.adapters.json_adapter", "from_json"),
-    # NetworkX adapter (optional dependency)
+    # NetworkX adapter
     "to_nx": ("annnet.adapters.networkx_adapter", "to_nx"),
     "from_nx": ("annnet.adapters.networkx_adapter", "from_nx"),
     "from_nx_only": ("annnet.adapters.networkx_adapter", "from_nx_only"),
+    # iGraph adapter
+    "to_igraph": ("annnet.adapters.igraph_adapter", "to_igraph"),
+    "from_igraph": ("annnet.adapters.igraph_adapter", "from_igraph"),
+    "from_igraph_only": ("annnet.adapters.igraph_adapter", "from_igraph_only"),
+    # graph-tool adapter
+    "to_graphtool": ("annnet.adapters.graphtool_adapter", "to_graphtool"),
+    "from_graphtool": ("annnet.adapters.graphtool_adapter", "from_graphtool"),
+    # Pytorch Geometric adapter
+    "to_pyg": ("annnet.adapters.pyg_adapter", "to_pyg"),
     # GraphML
-    "to_graphml": ("annnet.adapters.GraphML_adapter", "to_graphml"),
-    "from_graphml": ("annnet.adapters.GraphML_adapter", "from_graphml"),
+    "to_graphml": ("annnet.io.GraphML_io", "to_graphml"),
+    "from_graphml": ("annnet.io.GraphML_io", "from_graphml"),
     # SIF
-    "to_sif": ("annnet.adapters.SIF_adapter", "to_sif"),
-    "from_sif": ("annnet.adapters.SIF_adapter", "from_sif"),
+    "to_sif": ("annnet.io.SIF_io", "to_sif"),
+    "from_sif": ("annnet.io.SIF_io", "from_sif"),
     # SBML (common direction)
-    "from_sbml": ("annnet.adapters.sbml_adapter", "from_sbml"),
-    # If you add export later: "to_sbml": ("annnet.adapters.sbml_adapter", "to_sbml"),
+    "from_sbml": ("annnet.io.sbml_io", "from_sbml"),
     # Parquet GraphDir
     "write_parquet_graphdir": (
-        "annnet.adapters.GraphDir_Parquet_adapter",
+        "annnet.io.GraphDir_Parquet_io",
         "write_parquet_graphdir",
     ),
     "read_parquet_graphdir": (
-        "annnet.adapters.GraphDir_Parquet_adapter",
+        "annnet.adaptioers.GraphDir_Parquet_io",
         "read_parquet_graphdir",
     ),
+    # CX2
+    "to_cx2" : ("annnet.io.cx2_io", "to_cx2"),
+    "from_cx2": ("annnet.io.cx2_io", "from_cx2"),
+    # JSON
+    "to_json" : ("annnet.io.json_io", "to_json"),
+    "from_json": ("annnet.io.json_io", "from_json"),
 }
 
 __all__ = sorted(set(list(_lazy_submodules) + list(_lazy_symbols)))
@@ -78,7 +94,6 @@ def __dir__() -> list[str]:
     return sorted(list(globals().keys()) + list(__all__))
 
 
-# Version: prefer internal, then fall back to distribution metadata
 try:
     from ._version import __version__  # type: ignore
 except Exception:
